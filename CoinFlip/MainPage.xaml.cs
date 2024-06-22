a1cb3bdf68a9dc881150b6e563c167ea09309de3
LuishEtec <luishbarreira@yahoo.com>
2024-06-22 14:26:32 -0300

    Adicionar arquivos de projeto.

---
+++ CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Threading;
+
+namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+        String stringMoeda = String.Empty;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void moedaSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            stringMoeda = (String)moedaSelect.SelectedItem;
+        }
+
+        private void moedaBtn_Clicked(object sender, EventArgs e)
+        {
+            Random rnd = new();
+            int rand = rnd.Next(2);
+
+            if (stringMoeda == string.Empty)
+            {
+                DisplayAlert("Cara ou coroa", "Selecione cara ou coroa para jogar", "ok");
+            }
+            else
+            {
+                if (rand == moedaSelect.SelectedIndex)
+                {
+                    moedaOut.Text = "Parabens você acertou! ";
+                    imgOut.Source = stringMoeda + ".png";
+                }
+                else
+                {
+                    if (moedaSelect.SelectedIndex == 0)
+                    {
+                        imgOut.Source = "coroa.png";
+                        moedaOut.Text = "Que pena você errou!";
+                    }
+                    else
+                    {
+                        imgOut.Source = "cara.png";
+                        moedaOut.Text = "Que pena você errou!";
+                    }
+                }
+            }
+        }
+    }
+}
